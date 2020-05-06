d930de7f1819bc9039d9002811664d7adf1221fe
David <r.david.reid@gmail.com>
2020-05-06 06:50:20 -0700

    lesson 15 pre-caching assets

--- sw.js
+++ sw.js
@@ -1,6 +1,29 @@
+const staticCacheName = 'site-static'
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
+
 // install service worker
 self.addEventListener('install', (event) => {
-  console.log('service worker has been installed');
+  // wait until makes the `install` step not terminate until async caching is complete.
+  event.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      // cache.add() adds a single resource
+      // cache.addAll() adds an array of resources
+      console.log('caching shell assets')
+      cache.addAll(assets)
+    })
+  )
 })
 
 // watch for activate service worker 
